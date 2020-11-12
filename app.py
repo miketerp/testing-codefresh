5db6771e0298e80f08ee7b546b381c68f235d81a
Peter <psh2kim@gmail.com>
2020-11-12 15:18:40 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.P("hello peter")
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)

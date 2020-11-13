effde34542963c19ac6dac467323482c8c6bb98f
Peter <psh2kim@gmail.com>
2020-11-12 19:12:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash(__name__)
-app.title = 'Hello peter6'
+app.title = 'Hello peter7'
 server = app.server
 
 app.layout = html.Div([
-    html.P("hello peter 6")
+    html.P("hello peter 7")
 ])
 
 
